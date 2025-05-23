e7a69e23eeaabd3fa21c49ce9e72b33675026015
Tobias Wälde <tobias.waelde@gmail.com>
2025-05-23 16:32:44 +0200

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { swaggerConfig, swaggerTheme } from './config/swagger';
 
 async function bootstrap() {
   // create NEST app
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+  // configuration
+  app.getHttpAdapter().getInstance().disable('x-powered-by');
+  app.setGlobalPrefix('/api');
+  app.enableShutdownHooks();
+  app.enableCors({ origin: '*' });
+
+  // enable validation + serialization
+  //TODO
+
+  // swagger
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document, { customCss: swaggerTheme });
+
+  // start app
+  await app.listen(3001);
+  console.log(process.env);
 }
 
 bootstrap();
